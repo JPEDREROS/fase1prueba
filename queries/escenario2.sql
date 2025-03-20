5addb9628a635efc95cb56481f85893099b6909f
JPEDREROS <jpedreros@seidoranalytics.com>
2025-03-20 17:25:26 -0500

    Update escenario2.sql

--- queries/escenario2.sql
+++ queries/escenario2.sql
@@ -5,7 +5,3 @@ CREATE OR ALTER TABLE TABLE1 (ID VARCHAR, NOMBRE VARCHAR);
 
 EXECUTE IMMEDIATE 'SELECT *
 FROM TABLE1';
-
-
---Ambientes en GitHub ()
---Repositorio final debe tener reglas (protecciones para el Branch, para los permisos en los ajustes)
