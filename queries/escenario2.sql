e5f8f25f113d3470569fded8830142c21cae8353
JPEDREROS <jpedreros@seidoranalytics.com>
2025-03-20 17:36:31 -0500

    Update escenario2.sql

--- queries/escenario2.sql
+++ queries/escenario2.sql
@@ -1,11 +1,7 @@
 USE DATABASE_GITHUB2;
 USE SCHEMA OBJETOSGITHUB;
 
-CREATE OR ALTER TABLE TABLE1("ID" VARCHAR, "NOMBRE" VARCHAR);
+CREATE OR ALTER TABLE TABLE1 (ID VARCHAR, NOMBRE VARCHAR);
 
 EXECUTE IMMEDIATE 'SELECT *
 FROM TABLE1';
-
-
---Ambientes en GitHub ()
---Repositorio final debe tener reglas (protecciones para el Branch, para los permisos en los ajustes)
